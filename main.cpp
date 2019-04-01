2ff3778a03929bb12c8bac0be678d1b1821fafcc
u211-14 <u211-14@public.mpei.local>
2019-04-01 12:17:12 +0300

    code: Оформили гистограмму

--- main.cpp
+++ main.cpp
@@ -94,11 +94,14 @@ svg_end()
 void svg_text(double left, double baseline, string text)
 {
     cout<<"<text x=' " << left << " ' y=' " << baseline << " '> " << text << "</text>";
-}
-
-void svg_rect(double x, double y, double width, double height)
+}void svg_rect(double x, double y, double width, double height)
 {
     cout<<"<rect x=' "<< x << " ' y=' " << y << " ' width=' " << width << " '  height=' " << height <<" ' />" ;
+}
+void svg_rect(double x, double y, double width, double height,
+        string stroke, string fill)
+{
+    cout <<"<rect x='" << x << "' y='" << y <<"' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "'/>";
 }
 void show_histogram_svg(const vector<size_t> bins)
 {
@@ -109,16 +112,18 @@ void show_histogram_svg(const vector<size_t> bins)
     const auto TEXT_WIDTH = 50;
     const auto BIN_HEIGHT = 30;
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-    svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
-    svg_rect(TEXT_WIDTH, 0, bins[0] * 10, BIN_HEIGHT);
+//svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
+    //svg_rect(TEXT_WIDTH, 0, bins[0] * 10, BIN_HEIGHT);
     double top = 0;
     for (size_t bin : bins)
     {
         const double bin_width = 10 * bin;
         svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "#ffeeee");
         top += BIN_HEIGHT;
     }
+
+
     svg_end();
 }
 
